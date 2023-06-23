3fd05f70b0b960858d58584667bef47ef94f6240
Jared <u202020239@upc.edu.pe>
2023-06-23 18:43:17 -0500

    feat(new treatment) Added new treatment funtion
    con fe

--- lib/data/remote/http_helper.dart
+++ lib/data/remote/http_helper.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:theraphy_physiotherapist_app/data/model/patient.dart';
 import 'package:theraphy_physiotherapist_app/data/model/physiotherapist.dart';
+import 'package:theraphy_physiotherapist_app/data/model/treatment.dart';
 import 'package:theraphy_physiotherapist_app/data/model/user.dart';
 
 import 'package:http/http.dart' as http;
@@ -135,4 +136,56 @@ class HttpHelper {
       //print(email + " " + password + " " + type);
     }
   }
+  
+  Future<List<Treatment>?> getTreatment() async {
+    const String endpoint = '/treatments'; // Ruta del endpoint específico
+    final String url = '$urlBase$endpoint'; // URL completo
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(response.body);
+      final List treatmentsMap = jsonResponse['content'];
+      final List<Treatment> treatments = 
+      treatmentsMap.map((map) => Treatment.fromJson(map)).toList();
+      return treatments;    
+    }else{
+      return null;
+    }
+  }
+
+  Future<List<Treatment>?> addTreatment(Treatment newTreatment, int physiotherapistId) async {
+    const String strartpoint = '/physiotherapists/';
+    const String endpoint = '/treatments';
+    final String url = '$urlBase$strartpoint$physiotherapistId$endpoint';
+
+  final Map<String, dynamic> requestBody = {
+    'id': newTreatment.id,
+    'title': newTreatment.title,
+    'description': newTreatment.description,
+    'sessionsQuantity': newTreatment.sessionsQuantity,
+    'physiotherapistId': physiotherapistId,
+    'photoUrl': newTreatment.photoUrl,
+  };
+
+  final encodedBody = json.encode(requestBody);
+
+  http.Response response = await http.post(
+    Uri.parse(url),
+    body: encodedBody,
+    headers: {'Content-Type': 'application/json'},
+  );
+
+  if (response.statusCode == 201) {
+    final jsonResponse = json.decode(response.body);
+    final List treatmentsMap = jsonResponse['content'];
+    final List<Treatment> treatments = treatmentsMap
+        .map((map) => Treatment.fromJson(map))
+        .toList();
+    return treatments;
+  } else {
+    return null;
+  }
+}
+  
 }
